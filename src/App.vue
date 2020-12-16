14851b88d8ecbb0bfd0f33ef30efde0d950d682e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-12-16 20:00:22 +0100

    Add emoji and some desc text

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@
     <div class="w-full max-w-sm mx-auto">
       <header class="mb-5">
         <h1 class="text-lg font-bold text-center text-gray-700">
-          mob timer
+          mobt ⏰
         </h1>
+        <p class="text-sm font-bold text-center text-gray-700">
+          the mob timer
+        </p>
       </header>
       <Timer />
       <Team />
